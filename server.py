9b367f44ee61d5a50c74c8937a080ad5fd504b81
kaiou11 <iamkaiou11@gmail.com>
2024-11-06 03:55:57 +0800

    third commit

--- server.py
+++ server.py
@@ -12,8 +12,17 @@ def index():
 @app.route('/weather')
 def get_weather():
     city = request.args.get('city')
+    # Check for empty string
+    if not bool(city.strip()):
+        city = "Kansas City"
     weather_data = get_current_weather(city)
-    return render_template("weather.html", title=weather_data['name'], status=weather_data['weather'][0].capitalize(), temp=f"{weather_data['main']['temp']:.1f}",feels_like=f"{weather_data['main']['feels_like']:.1f}")
+    if not weather_data['cod'] == 200:
+        return render_template('city-not-found.html')
+    return render_template("weather.html", 
+                           title=weather_data['name'], 
+                           status=weather_data['weather'][0]['description'].capitalize(), 
+                           temp=f"{weather_data['main']['temp']:.1f}",
+                           feels_like=f"{weather_data['main']['feels_like']:.1f}")
 
 if __name__ == "__main__":
     serve(app, host="0.0.0.0", port=8000)
